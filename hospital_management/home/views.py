c715696e1391fc20d7ee82744ffaf9e4092377ff
prince-0303 <princebinil2003@gmail.com>
2025-07-16 17:57:41 +0530

    updated the department model

--- hospital_management/home/views.py
+++ hospital_management/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+from .models import Departments
 # Create your views here.
 
 def index(request) :
@@ -20,4 +20,7 @@ def contact(request) :
     return render(request, "contact.html")
 
 def department(request) :
-    return render(request, "department.html")
+    dict_dept ={
+        'dept' : Departments.objects.all()
+    }
+    return render(request, "department.html", dict_dept)
