42b646a93131f48676cba03f445013c40419e844
prince-0303 <princebinil2003@gmail.com>
2025-07-17 18:44:40 +0530

    added doctors model

--- hospital_management/home/models.py
+++ hospital_management/home/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Departments (models.Model):
     dep_name = models.CharField(max_length=100)
     dep_description = models.TextField()
+
+class Doctors (models.Model):
+    doc_name = models.CharField(max_length=100)
+    doc_speciality = models.CharField(max_length=150)
+    dep_name = models.ForeignKey(Departments, on_delete= models.CASCADE)
+    doc_img = models.ImageField(upload_to='doctors')
